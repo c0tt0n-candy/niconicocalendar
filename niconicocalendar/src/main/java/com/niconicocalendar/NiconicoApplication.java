e8f363cf0104d461b3c5d4e88aa6253d8fb3c441
Yutaka Okubo <yutaka@Yutaka-Air.local>
2016-04-28 23:12:28 +0900

    edit Application

---
+++ niconicocalendar/src/main/java/com/niconicocalendar/NiconicoApplication.java
@@ -0,0 +1,11 @@
+package com.niconicocalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiconicoApplication {
+    public static void main( String[] args ) {
+		SpringApplication.run(NiconicoApplication.class, args);
+    }
+}
