ae4199fc6af755e92ae70a177efa25a8b1241719
Yutaka Okubo <yutaka@Yutaka-Air.local>
2016-04-28 23:17:55 +0900

    edit the name of Application

--- niconicocalendar/src/main/java/com/niconicocalendar/Application.java
+++ niconicocalendar/src/main/java/com/niconicocalendar/Application.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NiconicoApplication {
+public class Application {
     public static void main( String[] args ) {
-		SpringApplication.run(NiconicoApplication.class, args);
+		SpringApplication.run(Application.class, args);
     }
 }
