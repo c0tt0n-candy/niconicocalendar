29cbe28a17c1483e0f59004a8978a9bf3267fe0b
Yutaka Okubo <yutaka@Yutaka-Air.local>
2016-04-29 17:26:48 +0900

    delete user

--- niconicocalendar/src/main/java/com/niconicocalendar/controller/IndexController.java
+++ niconicocalendar/src/main/java/com/niconicocalendar/controller/IndexController.java
@@ -82,5 +82,41 @@ public class IndexController {
 		return "niconico";
 	}
 	
+	@RequestMapping(value = "/delete")
+	public String deleteUser(Model model, @RequestParam("userId") String userId) {
+		
+		// ユーザー削除
+		jdbcTemplate.update("delete from user_tbl where userId=?", userId);
+		int count = jdbcTemplate.queryForObject("select count(*) from feelings_history_tbl where userId=?",
+				Integer.class, userId);
+		if (count>0){
+			jdbcTemplate.update("delete from feelings_history_tbl where userId=?", userId);
+		}
+		
+		// カレンダー取得
+		Calendar calendar = Calendar.getInstance();
+		int nowYear = calendar.get(Calendar.YEAR);
+		int nowMonth = calendar.get(Calendar.MONTH) + 1;
+		int nowDay = calendar.get(Calendar.DATE);
+
+		model.addAttribute("dispYear", nowYear);
+		model.addAttribute("dispMonth", nowMonth);
+
+		calendar.set(nowYear, nowMonth - 1, 1);
+		int lastDay = calendar.getActualMaximum(Calendar.DATE);
+		model.addAttribute("lastDay", lastDay);
+		
+		// ユーザーを取得
+		List<User> user = jdbcTemplate.query("select * from user_tbl",
+				(rs, rowNum) -> new User(rs.getInt("userId"), rs.getString("username")));
+		model.addAttribute("user", user);
+		
+		// 履歴を取得
+		List<Feelings> feelings = jdbcTemplate.query("select userId, day, feelingId from feelings_history_tbl where year=? and month=?",
+				(rs, rowNum) -> new Feelings(rs.getInt("userId"), rs.getInt("day"), rs.getInt("feelingId")), nowYear, nowMonth);
+		model.addAttribute("feelingHistory", feelings);
+		
+		return "niconico";
+	}
 	
 }
