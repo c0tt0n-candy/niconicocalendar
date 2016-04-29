2f763a011b3990ccdb3a96d4e31d63ab1c0554dc
Yutaka Okubo <yutaka@Yutaka-Air.local>
2016-04-29 17:08:57 +0900

    regist user

--- niconicocalendar/src/main/java/com/niconicocalendar/controller/IndexController.java
+++ niconicocalendar/src/main/java/com/niconicocalendar/controller/IndexController.java
@@ -8,6 +8,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.niconicocalendar.Feelings;
 import com.niconicocalendar.User;
@@ -46,4 +47,40 @@ public class IndexController {
 		
 		return "niconico";
 	}
+	
+	
+	@RequestMapping(value = "/regist")
+	public String registerUser(Model model, @RequestParam("name") String name) {
+		
+		// ユーザー追加
+		int maxUserId = jdbcTemplate.queryForObject("select max(userId) from user_tbl", Integer.class);
+		jdbcTemplate.update("insert into user_tbl(userId, username) VALUES (?,?)", maxUserId + 1, name);
+		
+		// カレンダー取得
+		Calendar calendar = Calendar.getInstance();
+		int nowYear = calendar.get(Calendar.YEAR);
+		int nowMonth = calendar.get(Calendar.MONTH) + 1;
+		int nowDay = calendar.get(Calendar.DATE);
+
+		model.addAttribute("dispYear", nowYear);
+		model.addAttribute("dispMonth", nowMonth);
+
+		calendar.set(nowYear, nowMonth - 1, 1);
+		int lastDay = calendar.getActualMaximum(Calendar.DATE);
+		model.addAttribute("lastDay", lastDay);
+		
+		// ユーザーを取得
+		List<User> user = jdbcTemplate.query("select * from user_tbl",
+				(rs, rowNum) -> new User(rs.getInt("userId"), rs.getString("username")));
+		model.addAttribute("user", user);
+		
+		// 履歴を取得
+		List<Feelings> feelings = jdbcTemplate.query("select userId, day, feelingId from feelings_history_tbl where year=? and month=?",
+				(rs, rowNum) -> new Feelings(rs.getInt("userId"), rs.getInt("day"), rs.getInt("feelingId")), nowYear, nowMonth);
+		model.addAttribute("feelingHistory", feelings);
+		
+		return "niconico";
+	}
+	
+	
 }
